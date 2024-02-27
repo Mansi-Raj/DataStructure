15042eb8428cb4b359c6151c993d499a1bd44b31
MANSI RAJ <157103259+Mansi-Raj@users.noreply.github.com>
2024-02-27 22:17:17 +0530

    Add files via upload

---
+++ src/Arrays/Array.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[]a=new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for (int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
