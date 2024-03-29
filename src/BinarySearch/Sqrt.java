3f6b249914bca3e21c32bb3ecd8844935375fa1d
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-03-29 15:16:05 +0530

    Add file

---
+++ src/BinarySearch/Sqrt.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class Sqrt {
+    public int mySqrt(int x){
+        if(x==0) return 0;
+        int start=1;
+        int end=x;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid==x/mid){
+                return mid;
+            }else if(mid>x/mid){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the no.: ");
+        int x=s.nextInt();
+        Sqrt o=new Sqrt();
+        System.out.print("Square root is "+o.mySqrt(x));
+        s.close();
+    }
+}
